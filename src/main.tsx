d11673a9d610aa514f8d35165bb29eecea6da2ef
Nicolas <nicolastimoteu@gmail.com>
2024-07-01 12:14:18 -0400

    😓

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
-// import './main.css'
-import { AppRoutes } from './routes.tsx'
+import { AppRoutes } from './routes'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
